1d5499b652c64be026c59b8109cec8a1f50799eb
Hamid <h55180d@gmail.com>
2025-08-16 23:12:21 +0300

    publish

--- store_manager/build.sh
+++ store_manager/build.sh
@@ -5,10 +5,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# إصلاح تعارض numpy و onnxruntime إذا كان مستخدمًا
-pip uninstall -y numpy onnxruntime || true
-pip install numpy==1.24.4 onnxruntime==1.17.1
-
 # ترحيل قاعدة البيانات
 python manage.py migrate --noinput
 
