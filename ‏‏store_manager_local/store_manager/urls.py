9e1d8e7c74ef0d5f4b12794557168c445c14843b
Hamid <h55180d@gmail.com>
2025-08-17 08:22:50 +0300

    4publish

--- ‏‏store_manager_local/store_manager/urls.py
+++
@@ -1,8 +0,0 @@
-# store_manager/urls.py
-from django.contrib import admin
-from django.urls import path, include # تأكد من إضافة include هنا
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')), # أضف هذا السطر
-]
